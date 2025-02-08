9307573c1aede4543fb7c009851b65358db2a2ad
djm5ms <djm5ms@virginia.edu>
2025-02-07 22:58:44 -0500

    broke

--- FlaskMain.py
+++ FlaskMain.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, url_for
+from PIL import Image
 import os
 from werkzeug.utils import secure_filename
 from werkzeug.middleware.shared_data import SharedDataMiddleware
+from SegCloth import segment_clothing
 
 
 app = Flask(__name__)
@@ -35,11 +37,15 @@ def upload_file():
         return 'No selected file'
     if file:
         filename = secure_filename(file.filename)
+        seg_file=segment_clothing(Image.open(f'uploads/{filename}'))
+        seg_filename='checker'+secure_filename(seg_file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
+        seg_file.save(os.path.join(app.config['UPLOAD_FOLDER'], seg_filename))
+        
         return render_template('index.html',
         message='File successfully uploaded',
-        image=url_for('uploaded_file', filename=filename)
+        image=url_for('uploaded_file', filename=filename),
+        image2=url_for('uploaded_file', filename=seg_filename)  
         )
 
 @app.route('/new-page')
