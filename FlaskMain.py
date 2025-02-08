80e3771e348ec748f502e2b2870ff98185c6d325
djm5ms <djm5ms@virginia.edu>
2025-02-07 19:07:46 -0500

    very start

--- FlaskMain.py
+++ FlaskMain.py
@@ -5,10 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
+@app.route('/', methods=['POST'])
+def renderImage():
+    
+    return render_template('index.html')
 
 @app.route('/new-page')
 def new_page():
     return '45'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
