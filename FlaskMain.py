38c876612d0a9c78b6ec837c3a0f8de01e679ad8
NicholasLyall <NicholasLyall@icloud.com>
2025-02-07 22:55:35 -0500

    saves the cropped

--- FlaskMain.py
+++ FlaskMain.py
@@ -4,6 +4,10 @@ from werkzeug.utils import secure_filename
 from werkzeug.middleware.shared_data import SharedDataMiddleware
 
 
+from SegCloth import segment_clothing
+
+from PIL import Image
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['UPLOAD_FOLDER_URL'] = '/uploads'
@@ -36,6 +40,12 @@ def upload_file():
     if file:
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        image = Image.open(filepath)
+
+        result = segment_clothing(img=image, clothes = ["Upper-clothes"])
+        result.save("segmentedBiker.png")
     
         return render_template('index.html',
         message='File successfully uploaded',
