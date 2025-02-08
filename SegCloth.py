4f2002fd985528f588bc372af9604d65d2d46bec
NicholasLyall <NicholasLyall@icloud.com>
2025-02-08 00:42:51 -0500

    changed some html

--- SegCloth.py
+++ SegCloth.py
@@ -7,34 +7,27 @@ import numpy as np
 segmenter = pipeline(model="mattmdjaga/segformer_b2_clothes")
 
 
-def segment_clothing(image, clothes=["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]):
-    # Convert image to RGBA if it's not already
-    if isinstance(image, str):
-        image = Image.open(image)
-    image = image.convert('RGBA')
-    
+def segment_clothing(img, clothes= ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]):
     # Segment image
-    segments = segmenter(image)
-    
+    segments = segmenter(img)
+
     # Create list of masks
     mask_list = []
     for s in segments:
-        if s['label'] in clothes:
+        if(s['label'] in clothes):
             mask_list.append(s['mask'])
-    
-    if not mask_list:
-        return image
-        
-    # Paste all masks on top of each other
+
+
+    # Paste all masks on top of eachother 
     final_mask = np.array(mask_list[0])
-    for mask in mask_list[1:]:
+    for mask in mask_list:
         current_mask = np.array(mask)
-        final_mask = np.clip(final_mask + current_mask, 0, 255)
+        final_mask = final_mask + current_mask
             
-    # Convert final mask to PIL image
-    final_mask = Image.fromarray(final_mask.astype(np.uint8), mode='L')
-    
+    # Convert final mask from np array to PIL image
+    final_mask = Image.fromarray(final_mask)
+
     # Apply mask to original image
-    image.putalpha(final_mask)
-    
-    return image
+    img.putalpha(final_mask)
+
+    return img
